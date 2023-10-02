9870474e98d331c1b00511a0b514fe855366ca5b
Zeliha Selcuk <zeliha777@hotmail.com>
2023-10-02 21:38:34 +0300

    day16 son

--- src/day16_ArrayList_ForEachLoop/C03_IndexOf.java
+++ src/day16_ArrayList_ForEachLoop/C03_IndexOf.java
@@ -1,2 +1,31 @@
-package day16_ArrayList_ForEachLoop;public class C03_IndexOf {
+package day16_ArrayList_ForEachLoop;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class C03_IndexOf {
+    public static void main(String[] args) {
+
+        List<String> isimler =new ArrayList<>();
+        isimler.add("Deniz");
+        isimler.add("Ömer");
+        isimler.add("Esra");
+        isimler.add("Hamza");
+        isimler.add("Başak");
+        isimler.add("Esra");
+
+        System.out.println(isimler); // [Deniz, Ömer, Esra, Hamza, Başak, Esra]
+
+        System.out.println(isimler.indexOf("Esra"));// 2  // 2 tane esra var , esra nın ilk index ini verir.
+        System.out.println(isimler.lastIndexOf("Esra")); // 5  // esra nın sonuncu indexini verir.
+
+        // olmayan isimin index ini istesek ??
+
+        System.out.println(isimler.indexOf("Ercan")); // -1 //  yok
+        System.out.println(isimler.indexOf("Azim")); // -1 //   yok
+        System.out.println(isimler.indexOf("Gülnur")); // -1 //   yok
+
+
+
+    }
 }
